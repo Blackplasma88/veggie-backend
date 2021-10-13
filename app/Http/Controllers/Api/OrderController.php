62ebfb40536fdafb227e6829cc7184052a30a04d
Artkungg <itthipon.d@ku.th>
2021-10-13 21:06:43 +0700

    delete Order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -68,6 +68,7 @@ class OrderController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $order = Order::findOrFail($id);
+        $order->delete();
     }
 }
